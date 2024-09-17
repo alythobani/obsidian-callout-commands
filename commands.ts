5fe6371481e4bc437a6ccde885dbf7a7a235aa10
Aly Thobani <athobani9@gmail.com>
2024-09-16 20:52:42 -0700

    Refactor out helper function `maybePerformActionOnSelectedText`

--- commands.ts
+++ commands.ts
@@ -1,34 +1,49 @@
-import { Command } from "obsidian";
+import { Command, Editor } from "obsidian";
 
 type EditorCheckCallback = Command["editorCheckCallback"];
 
-const turnSelectedLinesIntoQuoteCallout: EditorCheckCallback = (checking, editor, _ctx) => {
+/**
+ * Perform an action on the selected text if there is any, and return whether the action was performed.
+ *
+ * @param checking Whether we are checking or performing the action (see `editorCheckCallback`)
+ * @param editor The editor instance
+ * @param action The editor action to perform on the selected text
+ */
+function maybePerformActionOnSelectedText(
+  checking: boolean,
+  editor: Editor,
+  action: (editor: Editor, selectedText: string) => void
+): boolean {
   const selectedText = editor.getSelection();
   if (!selectedText) return false; // Don't show the command if no text is selected
   if (checking) return true;
-  const replacedText = selectedText.replace(/^/gm, "> ");
-  editor.replaceSelection(`\n\n> [!quote] Quote\n${replacedText}\n\n`);
+  action(editor, selectedText);
   return true;
+}
+
+const maybeTurnSelectedLinesIntoQuoteCallout: EditorCheckCallback = (checking, editor, _ctx) => {
+  return maybePerformActionOnSelectedText(checking, editor, (editor, selectedText) => {
+    const replacedText = selectedText.replace(/^/gm, "> ");
+    editor.replaceSelection(`\n\n> [!quote] Quote\n${replacedText}\n\n`);
+  });
 };
 
-const removeCalloutFromSelectedLines: EditorCheckCallback = (checking, editor, _ctx) => {
-  const selectedText = editor.getSelection();
-  if (!selectedText) return false; // Don't show the command if no text is selected
-  if (checking) return true;
-  const removedCallout = selectedText.replace(/^> (\[!\w+\] )?/gm, "");
-  editor.replaceSelection(removedCallout);
-  return true;
+const maybeRemoveCalloutFromSelectedLines: EditorCheckCallback = (checking, editor, _ctx) => {
+  return maybePerformActionOnSelectedText(checking, editor, (editor, selectedText) => {
+    const removedCallout = selectedText.replace(/^> (\[!\w+\] )?/gm, "");
+    editor.replaceSelection(removedCallout);
+  });
 };
 
 export const allCommands: Command[] = [
   {
     id: "turn-selected-lines-into-quote-callout",
     name: "Turn Selected Lines into Quote Callout",
-    editorCheckCallback: turnSelectedLinesIntoQuoteCallout,
+    editorCheckCallback: maybeTurnSelectedLinesIntoQuoteCallout,
   },
   {
     id: "remove-callout-from-selected-lines",
     name: "Remove Callout from Selected Lines",
-    editorCheckCallback: removeCalloutFromSelectedLines,
+    editorCheckCallback: maybeRemoveCalloutFromSelectedLines,
   },
 ];
