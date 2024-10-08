c31f3821c0ec721f1ff705c69df3ff3bc99df06e
Aly Thobani <athobani9@gmail.com>
2024-10-08 16:22:41 -0700

    remove unused function

--- src/callouts/calloutManager.ts
+++ src/callouts/calloutManager.ts
@@ -52,25 +52,3 @@ export function getAllCalloutIDsFromCalloutManager(
   const allCalloutIDs = allCallouts.map((callout) => callout.id);
   return allCalloutIDs;
 }
-
-/**
- * Gets the callout IDs that were added/removed in the new set of callout IDs returned by the
- * Callout Manager API compared to the old set of callout IDs.
- */
-export function getAddedAndRemovedCalloutIDs({
-  calloutManager,
-  oldCalloutIDs,
-}: {
-  calloutManager: CalloutManagerOwnedHandle;
-  oldCalloutIDs: Set<CalloutID>;
-}): {
-  newCalloutIDsSet: Set<CalloutID>;
-  addedCalloutIDs: CalloutID[];
-  removedCalloutIDs: CalloutID[];
-} {
-  const newCalloutIDs = getAllCalloutIDsFromCalloutManager(calloutManager);
-  const newCalloutIDsSet = new Set(newCalloutIDs);
-  const addedCalloutIDs = newCalloutIDs.filter((id) => !oldCalloutIDs.has(id));
-  const removedCalloutIDs = Array.from(oldCalloutIDs).filter((id) => !newCalloutIDsSet.has(id));
-  return { addedCalloutIDs, removedCalloutIDs, newCalloutIDsSet };
-}
